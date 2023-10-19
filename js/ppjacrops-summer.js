ec9562f804edad268d7ad9c709bb2fa1e1dec02f
TylerDickard <dickart@bgsu.edu>
2023-10-18 22:27:36 -0400

    finish summer crops

---
+++ js/ppjacrops-summer.js
@@ -0,0 +1,575 @@
+var ppjaSummerCrops = {
+    "adzukibean": {
+        "name": "Adzuki Bean",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "adzukibean.png",
+        "seeds": {
+        "pierre": 50,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 9,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 90,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "aloe": {
+        "name": "Aloe",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "aloe.png",
+        "seeds": {
+        "pierre": 0,
+        "joja": 0,
+        "special": 100,
+        "specialLoc": "Harvey Year 2+",
+        "specialUrl": "https://stardewvalleywiki.com/Harvey"
+        },
+        "growth": {
+        "initial": 4,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 140,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "asparagus": {
+        "name": "Asparagus",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "asparagus.png",
+        "seeds": {
+        "pierre": 20,
+        "joja": 0,
+        "special": 20,
+        "specialLoc": "Pierre Year 4+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 5,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 35,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "blueagave": {
+        "name": "Blue Agave",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "blueagave.png",
+        "seeds": {
+        "pierre": 0,
+        "joja": 0,
+        "special": 200,
+        "specialLoc": "Oasis Year 3+",
+        "specialUrl": "https://stardewvalleywiki.com/Oasis"
+        },
+        "growth": {
+        "initial": 12,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 300,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "butternutsquash": {
+        "name": "Butternut Squash",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "butternutsquash.png",
+        "seeds": {
+        "pierre": 180,
+        "joja": 0,
+        "special": 180,
+        "specialLoc": "Pierre Year 3+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 7,
+        "regrow": 5
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0.2,
+        "price": 440,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "cassava": {
+        "name": "Cassava",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "cassava.png",
+        "seeds": {
+        "pierre": 60,
+        "joja": 0,
+        "special": 60,
+        "specialLoc": "Pierre Year 2+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 7,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 80,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "chickpea": {
+        "name": "Chickpea",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "chickpea.png",
+        "seeds": {
+        "pierre": 200,
+        "joja": 0,
+        "special": 200,
+        "specialLoc": "Pierre Year 4+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 10,
+        "regrow": 4
+        },
+        "produce": {
+        "extra": 1,
+        "extraPerc": 0,
+        "price": 55,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "chives": {
+        "name": "Chives",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "chives.png",
+        "seeds": {
+        "pierre": 20,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 5,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 35,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "cottonboll": {
+        "name": "Cotton Boll",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "cottonboll.png",
+        "seeds": {
+        "pierre": 80,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 7,
+        "regrow": 3
+        },
+        "produce": {
+        "extra": 1,
+        "extraPerc": 0,
+        "price": 65,
+        "jarType": "Jelly",
+        "kegType": "Wine"
+        }
+    },
+    "cucumber": {
+        "name": "Cucumber",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "cucumber.png",
+        "seeds": {
+        "pierre": 65,
+        "joja": 0,
+        "special": 65,
+        "specialLoc": "Pierre Year 2+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 11,
+        "regrow": 3
+        },
+        "produce": {
+        "extra": 1,
+        "extraPerc": 0.2,
+        "price": 90,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "ginger": {
+        "name": "Ginger",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "ginger.png",
+        "seeds": {
+        "pierre": 0,
+        "joja": 0,
+        "special": 35,
+        "specialLoc": "Oasis (After Boat is Done)",
+        "specialUrl": "https://stardewvalleywiki.com/Oasis"
+        },
+        "growth": {
+        "initial": 7,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 60,
+        }
+    },
+    "gooseberry": {
+        "name": "Gooseberry",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "gooseberry.png",
+        "seeds": {
+        "pierre": 80,
+        "joja": 0,
+        "special": 80,
+        "specialLoc": "Pierre Year 2+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 13,
+        "regrow": 3
+        },
+        "produce": {
+        "extra": 2,
+        "extraPerc": 0.1,
+        "price": 20,
+        "jarType": "Jelly",
+        "kegType": "Wine"
+        }
+    },
+    "greenpeas": {
+        "name": "Green Peas",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "greenpeas.png",
+        "seeds": {
+        "pierre": 50,
+        "joja": 0,
+        "special": 50,
+        "specialLoc": "Pierre Year 4+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 8,
+        "regrow": 3
+        },
+        "produce": {
+        "extra": 2,
+        "extraPerc": 0.2,
+        "price": 30,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "kiwi": {
+        "name": "Kiwi",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "kiwi.png",
+        "seeds": {
+        "pierre": 300,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 21,
+        "regrow": 3
+        },
+        "produce": {
+        "extra": 1,
+        "extraPerc": 0.1,
+        "price": 100,
+        "jarType": "Jelly",
+        "kegType": "Wine"
+        }
+    },
+    "lettuce": {
+        "name": "Lettuce",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "lettuce.png",
+        "seeds": {
+        "pierre": 55,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 6,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 65,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "navybean": {
+        "name": "Navy Bean",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "navybean.png",
+        "seeds": {
+        "pierre": 80,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 10,
+        "regrow": 3
+        },
+        "produce": {
+        "extra": 2,
+        "extraPerc": 0.1,
+        "price": 15,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "okra": {
+        "name": "Okra",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "okra.png",
+        "seeds": {
+        "pierre": 160,
+        "joja": 0,
+        "special": 160,
+        "specialLoc": "Pierre Year 3+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 10,
+        "regrow": 4
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0.2,
+        "price": 60,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "oregano": {
+        "name": "Oregano",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "oregano.png",
+        "seeds": {
+        "pierre": 30,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 4,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 50,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "perillaleaf": {
+        "name": "Perilla Leaf",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "perillaleaf.png",
+        "seeds": {
+        "pierre": 30,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 4,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 50,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "quinoa": {
+        "name": "Quinoa",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "quinoa.png",
+        "seeds": {
+        "pierre": 20,
+        "joja": 0,
+        "special": 20,
+        "specialLoc": "Pierre Year 4+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 6,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 30,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "raspberry": {
+        "name": "Raspberry",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "raspberry.png",
+        "seeds": {
+        "pierre": 60,
+        "joja": 0,
+        "special": 60,
+        "specialLoc": "Pierre Year 2+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 10,
+        "regrow": 4
+        },
+        "produce": {
+        "extra": 3,
+        "extraPerc": 0.2,
+        "price": 30,
+        "jarType": "Jelly",
+        "kegType": "Wine"
+        }
+    },
+    "sugarcane": {
+        "name": "Sugar Cane",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "sugarcane.png",
+        "seeds": {
+        "pierre": 0,
+        "joja": 0,
+        "special": 110,
+        "specialLoc": "The Oasis",
+        "specialUrl": "https://stardewvalleywiki.com/Oasis"
+        },
+        "growth": {
+        "initial": 10,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 230,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "sulfurshelfmushroom": {
+        "name": "Sulfur Shelf Mushroom",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "sulfurshelfmushroom.png",
+        "seeds": {
+        "pierre": 500,
+        "joja": 0,
+        "special": 500,
+        "specialLoc": "Pierre Year 4+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 10,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 2,
+        "extraPerc": 0.1,
+        "price": 180,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "wasabiroot": {
+        "name": "Wasabi Root",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "wasabiroot.png",
+        "seeds": {
+        "pierre": 100,
+        "joja": 0,
+        "special": 0,
+        "specialLoc": "",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 8,
+        "regrow": 0
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 165,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    },
+    "zucchini": {
+        "name": "Zucchini",
+        "url": "https://mouseypounds.github.io/ppja-ref/crops.html",
+        "img": "zucchini.png",
+        "seeds": {
+        "pierre": 225,
+        "joja": 0,
+        "special": 225,
+        "specialLoc": "Pierre Year 3+",
+        "specialUrl": ""
+        },
+        "growth": {
+        "initial": 14,
+        "regrow": 2
+        },
+        "produce": {
+        "extra": 0,
+        "extraPerc": 0,
+        "price": 175,
+        "jarType": "Pickles",
+        "kegType": "Juice"
+        }
+    }
+}
